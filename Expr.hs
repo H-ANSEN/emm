f2da660ec60268ebd76599e41a39efb5997117f3
Teague Hansen <thanse23@asu.edu>
2024-11-16 13:43:14 -0500

    extract expression implementation to individual file

---
+++ Expr.hs
@@ -0,0 +1,86 @@
+module Expr 
+  ( Expr(..)
+  , Rule(..)
+  , applyAll
+  )
+  where
+
+import Prelude hiding (lookup)
+import Text.Printf (printf)
+import Control.Applicative (liftA2)
+import Data.List (intercalate)
+import Data.Maybe (fromMaybe)
+import Data.Map.Strict (Map)
+import Data.Map.Merge.Strict (merge, preserveMissing, zipWithMaybeMatched)
+
+import qualified Data.Map.Strict as Map
+
+data Expr
+  = Sym String
+  | Fun String [Expr]
+  deriving Eq
+
+data Rule = Rule
+  { hd :: Expr
+  , body :: Expr
+  }
+
+type Bindings = Map String Expr
+
+instance Show Expr where
+  show (Sym s) = s
+  show (Fun name args) = printf "%s(%s)" name (intercalate ", " (map show args))
+
+instance Show Rule where
+  show Rule { hd, body } = show hd <> " = " <> show body
+
+-- Merge two bindings together removing duplicate keys binding different values
+mergeBindings :: Bindings -> Bindings -> Bindings
+mergeBindings = merge preserveMissing 
+                      preserveMissing 
+                      (zipWithMaybeMatched combine)
+  where
+    combine _ v1 v2 | v1 == v2  = Just v1
+                    | otherwise = Nothing
+
+-- Substitue bound values in the given expression using the given bindings
+substBindings :: Bindings -> Expr -> Expr
+substBindings bindings expr =
+  case expr of
+    Sym name -> fromMaybe expr (Map.lookup name bindings)
+    Fun name args ->
+      let new_name = 
+            case Map.lookup name bindings of
+              Nothing -> name
+              Just (Sym new_name) -> new_name
+              _ -> undefined 
+      in
+      let new_args = map (substBindings bindings) args in
+      Fun new_name new_args
+
+-- Pattern match across two expressions creating a set of bindings between the
+-- two
+match :: Expr -> Expr -> Bindings
+match pattern value =
+  case (pattern, value) of
+    (Sym s1, _) -> Map.singleton s1 value
+    (Fun name1 args1, Fun name2 args2) ->
+      if name1 /= name2 || length args1 /= length args2 
+         then Map.empty
+         else foldr1 mergeBindings $ zipWith match args1 args2
+    _ -> Map.empty 
+
+-- Given a rule and an expression attempt to apply the rule to the expression
+applyAll :: Rule -> Expr -> Expr
+applyAll rule expr =
+  let bindings = match (hd rule) expr in
+      if Map.null bindings then
+        case expr of
+          Sym _ -> expr
+          Fun name args -> Fun name (map (applyAll rule) args)
+      else
+        substBindings bindings (body rule)
+
+printBindings :: Bindings -> IO ()
+printBindings b =
+   mapM_ (putStrLn . (\(k, v) -> show k <> " => " <> show v)) (Map.toList b)
